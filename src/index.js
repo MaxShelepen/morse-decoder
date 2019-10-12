6194e0f6c55582feefb3da43ab24cff4033cd3c4
Max <maxshelepen25@gmail.com>
2019-10-12 13:03:43 +0300

    feat:complite task

--- src/index.js
+++ src/index.js
@@ -38,9 +38,30 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    MORSE_TABLE[''] = ' ';
+    let char_10 = /.{1,10}/g; 
+    let char_2 = /\d{1,2}/g; 
+    let space = /\*{1,10}/g; 
+    
+    let letterArr = expr.match(char_10);
+    let result = '';
 
+    letterArr.forEach(function(element) {
+       if(!space.test(symbolsArr = element)){
+        symbolsArr = element.match(char_2)} else{
+        ['space']}
+      
+      const decodedSymbols = symbolsArr.map(function(item) {
+        if (item === "10"){ return ".";}
+        if (item === "11") {return "-";}
+        if (item === "space"){return '';}
+        if (item === '00') {return '';}
+      });
+      
+      result += MORSE_TABLE[decodedSymbols.join('')]
+    });
+    return result;
+  }
 module.exports = {
-    decode
+  decode
 }
