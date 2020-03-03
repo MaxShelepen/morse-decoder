3a01ab8d5ed927bc756f477aca4921e9864a5ef2
maxshelepen <maxshelepen25@gmail.com>
2020-03-03 21:28:05 +0300

    Solved

--- src/index.js
+++ src/index.js
@@ -1,61 +1,63 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+  '.-': 'a',
+  '-...': 'b',
+  '-.-.': 'c',
+  '-..': 'd',
+  '.': 'e',
+  '..-.': 'f',
+  '--.': 'g',
+  '....': 'h',
+  '..': 'i',
+  '.---': 'j',
+  '-.-': 'k',
+  '.-..': 'l',
+  '--': 'm',
+  '-.': 'n',
+  '---': 'o',
+  '.--.': 'p',
+  '--.-': 'q',
+  '.-.': 'r',
+  '...': 's',
+  '-': 't',
+  '..-': 'u',
+  '...-': 'v',
+  '.--': 'w',
+  '-..-': 'x',
+  '-.--': 'y',
+  '--..': 'z',
+  '.----': '1',
+  '..---': '2',
+  '...--': '3',
+  '....-': '4',
+  '.....': '5',
+  '-....': '6',
+  '--...': '7',
+  '---..': '8',
+  '----.': '9',
+  '-----': '0',
 };
 
 function decode(expr) {
-    MORSE_TABLE[''] = ' ';
-    let elem1 = /.{1,10}/g; 
-    let elem2 = /\d{1,2}/g; 
-    let space = /\*{1,10}/g; 
+  
+  MORSE_TABLE[''] = ' ';
+  
+    let char_10 = /.{1,10}/g; //regexp to get all 10 paired characters
+    let char_2 = /\d{1,2}/g; //regexp to get all 2 paired characters
+    let whiteSpace = /\*{1,10}/g; //regexp to get * chars
     
-    let letterArr = expr.match(elem1);
+    let letterArr = expr.match(char_10);
     let result = '';
 
-    letterArr.forEach(function(element) {
-       if(!space.test(element)){
-        symbolsArr = element.match(elem2)} else{
-        ['space']}
+    letterArr.forEach(element => {
+      let symbolsArr = !whiteSpace.test(element) ?
+        element.match(char_2) :
+        ['whitespace'];
       
-      const decodedSymbols = symbolsArr.map(function(item) {
-        if (item === "10"){ return ".";}
-        if (item === "11") {return "-";}
-        if (item === "space"){return '';}
-        if (item === '00') {return '';}
+      const decodedSymbols = symbolsArr.map(item => {
+        if (item === "10") return ".";
+        if (item === "11") return "-";
+        if (item === "whitespace") return '';
+        if (item === '00') return '';
       });
       
       result += MORSE_TABLE[decodedSymbols.join('')]
